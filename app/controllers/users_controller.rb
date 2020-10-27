fcdc2d4c34dfdc193141697eac361a7b2194ffce
naoki.hannzawa <taro.kirameki@techacademy.jp>
2020-10-27 05:50:23 +0000

    user signup

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,35 @@
+class UsersController < ApplicationController
+  #一覧を表示
+  def index
+    @users = User.order(id: :desc).page(params[:page]).per(25)
+  end
+ #レコードの内容を表示
+  def show
+    @user = User.find(params[:id])
+  end
+ #リソースを新規作成
+  def new
+    @user = User.new
+  end
+ #保存
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:success] = 'ユーザを登録しました。'
+      #create アクション実行後に更に show アクションが実行され、
+      #show.html.erb が呼ばれる
+      redirect_to @user
+    else
+      flash.now[:danger] = 'ユーザの登録に失敗しました。'
+      #単に users/new.html.erb を表示するだけ
+      render :new
+    end
+  end
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   #root：アクセスして一番最初のコントローラへの指示.
   #主にトップページなどに用いられる
   root to: 'toppages#index'
+  
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
 end
